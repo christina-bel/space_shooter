8f257472a38adc0f62e6bcf83b82afab19226aac
christina-bel <muza-kris@mail.ru>
2025-01-11 20:00:28 +0300

    Add enemy

--- lib/game/space_shooter_game.dart
+++ lib/game/space_shooter_game.dart
@@ -1,8 +1,10 @@
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
+import 'package:flame/experimental.dart';
 import 'package:flame/game.dart';
 import 'package:flame/parallax.dart';
 import 'package:flutter/material.dart';
+import 'package:space_shooter/models/enemy.dart';
 import 'package:space_shooter/models/player.dart';
 
 class SpaceShooterGame extends FlameGame with PanDetector {
@@ -26,6 +28,14 @@ class SpaceShooterGame extends FlameGame with PanDetector {
 
     player = Player();
     add(player);
+
+    add(
+      SpawnComponent(
+        factory: (_) => Enemy(),
+        period: 1,
+        area: Rectangle.fromLTWH(0, 0, size.x, -Enemy.enemySize),
+      ),
+    );
   }
 
   @override
